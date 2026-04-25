cb8821318161cc4b07a9f074c6f438c56377b735
tiger2131231 <duongvuthai2018@gmail.com>
2026-04-25 21:16:27 +0700

    update tool and toolBorrowing

--- project_backend_thermoelectric/src/main/java/com/example/project_backend_thermoelectric/entity/ToolBorrowing.java
+++ project_backend_thermoelectric/src/main/java/com/example/project_backend_thermoelectric/entity/ToolBorrowing.java
@@ -1,6 +1,8 @@
 package com.example.project_backend_thermoelectric.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -17,12 +19,15 @@ public class ToolBorrowing {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Số lượng mượn không được để trống")
+    @Min(value = 1, message = "Số lượng mượn phải ít nhất là 1")
     private Integer quantity;
 
     private String status;
 
     private String note;
 
+    @NotNull(message = "Ngày mượn không được để trống")
     @Column(name = "borrow_date")
     private LocalDateTime borrowDate;
 
@@ -32,10 +37,12 @@ public class ToolBorrowing {
     @Column(name = "return_date")
     private LocalDateTime returnDate;
 
+    @NotNull(message = "Dụng cụ không được để trống")
     @ManyToOne
     @JoinColumn(name = "tool_id")
     private Tool tool;
 
+    @NotNull(message = "Nhân viên mượn không được để trống")
     @ManyToOne
     @JoinColumn(name = "employee_id")
     private Employee employee;
