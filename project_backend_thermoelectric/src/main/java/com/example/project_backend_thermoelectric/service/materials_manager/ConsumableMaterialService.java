033860a69eeaf1a46906cd27996b3a331f32df5b
NgThien16 <thiennngcd220423@fpt.edu.vn>
2026-05-16 19:34:35 +0700

    save code

--- project_backend_thermoelectric/src/main/java/com/example/project_backend_thermoelectric/service/materials_manager/ConsumableMaterialService.java
+++ project_backend_thermoelectric/src/main/java/com/example/project_backend_thermoelectric/service/materials_manager/ConsumableMaterialService.java
@@ -3,6 +3,8 @@ package com.example.project_backend_thermoelectric.service.materials_manager;
 import com.example.project_backend_thermoelectric.entity.ConsumableMaterial;
 import com.example.project_backend_thermoelectric.repository.materials_manager.IConsumableMaterialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -42,7 +44,9 @@ public class ConsumableMaterialService implements IConsumableMaterialService {
     }
 
     @Override
-    public List<ConsumableMaterial> findByNameOrCode(String keyword) {
-        return consumableMaterialRepository.searchConsumableMaterial(keyword);
+    public Page<ConsumableMaterial> findByNameOrCode(String code, String name, Pageable pageable) {
+        return consumableMaterialRepository.searchConsumableMaterial(code, name, pageable);
     }
+
+
 }
